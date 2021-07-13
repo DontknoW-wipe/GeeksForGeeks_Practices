ae7df12091273b445773a85b9b068a4a24e84dda
DontknoW-wipe <57296632+DontknoW-wipe@users.noreply.github.com>
2021-07-13 14:02:11 +0530

    Create Kth Smallest Factor.cpp

---
+++ Kth Smallest Factor.cpp
@@ -0,0 +1,35 @@
+/*
+
+Kth Smallest Factor
+Medium Accuracy: 42.71% Submissions: 2427 Points: 4
+
+GIven two positive integers N and K. You have to find the Kth smallest factor of N. A factor of N is a positive integer which divides N. Output the Kth smallest factor of N if it exists otherwise print -1.
+
+ 
+
+Example 1:
+
+Input : N = 4 , K = 2
+Output: 2
+Explanation:
+All factors of 4 are 1,2 and 4. Out of
+these 2 is the 2nd smallest.
+
+Example 2:
+
+Input : N = 4 , K = 3
+Output: 4
+Explanation:
+All factors of 4 are 1,2 and 4. Out of
+these 4 is the 3rd smallest.
+
+ 
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function kThSmallestFactor() which takes 2 Integers N and K as input and returns the answer.
+
+ 
+
+Expected Time Complexity: O(sqrt(N))
+Expected Auxiliary Space: O(1)
+*/
